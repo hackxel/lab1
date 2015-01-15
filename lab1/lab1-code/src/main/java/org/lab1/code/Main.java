b5eac6fe6a80d2d3a77ce0005cfb824683e39a49
Hackxel <darkax99@hotmail.com>
2015-01-15 14:49:08 -0500

    meh first thing and stuff

---
+++ lab1/lab1-code/src/main/java/org/lab1/code/Main.java
@@ -0,0 +1,11 @@
+package org.lab1.code;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Room room = new Room();
+		System.out.println("Is room available? " + room.isAvailable());
+	}
+
+}
+
---
+++ lab1/lab1-code/src/main/java/org/lab1/code/Room.java
@@ -0,0 +1,10 @@
+package org.lab1.code;
+
+public class Room {
+
+	public boolean isAvailable() {
+		return false;
+	}
+
+}
+
